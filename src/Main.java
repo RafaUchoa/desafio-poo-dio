f42fe1e800220cde0b053a29a340e15b47cb8485
Rafael Uchôa <rafa.uchoa@gmail.com>
2026-01-05 19:37:44 -0300

    Criação das classes de curso e mentoria

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Curso completo de java");
+        curso1.setCargaHoraria(8);
+
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Python");
+        curso2.setDescricao("Curso muito bacana");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Muito boa");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
